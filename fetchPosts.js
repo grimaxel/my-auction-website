5b8e2de65fcd2d43c3e37b6ee712470def724ffe
grimaxel <axeliusgrim@gmail.com>
2024-10-01 15:48:29 +0200

    Update fetchPosts.js

--- fetchPosts.js
+++ fetchPosts.js
@@ -1,4 +1,4 @@
-const apiKey = patglkGR9KzREBGp7.7aa5ca630907eee099fb4e09600e040a208c3a707a8a6e0c7230b418331b3dd8;  // Replace with your Airtable Personal Access Token
+const apiKey = process.env.AIRTABLE_API_KEY;  // Replace with your Airtable Personal Access Token
 const baseId = 'appmwUSpnRzUv06sj';  // Your Airtable Base ID
 const tableName = 'Table%201';  // Replace with your Table Name (URL-encoded)
 
